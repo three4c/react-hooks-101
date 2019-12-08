698eb2d0b5fdfb4b845626f84be62ef20186c1c6
three4c <three4c@gmail.com>
2019-12-08 23:52:43 +0900

    refactaling

---
+++ src/components/Events.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Evnet from "./Event";
+
+const Events = ({ state, dispatch }) => {
+  return (
+    <>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event, index) => (
+            <Evnet key={index} event={event} dispatch={dispatch} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Events;
